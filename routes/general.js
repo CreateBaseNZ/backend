803d4949eb4a4fa62e3ef0d21beb27cdc539a07a
Nicole Ramirez <nicoleramirez@Nicoles-MBP-2.hub>
2019-12-15 07:14:02 +1300

    modified general.js file

--- routes/general.js
+++ routes/general.js
@@ -1,10 +1,34 @@
-//set server to localhost:3000
-var express = require('express');
-var app = express();
-app.set('port', process.env.PORT || 3000);
+// requiring the necessary modules
+const http = require('http');
 
-// use bodyparser to read form data
-var bodyparser = require('body-parser')
-app.use(bodyparser.urlencoded({extended: true}));
+const mongoose = require("mongoose");
 
-var db = require()
+const hostname = '192.168.1.202';
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+// load User and UserProfile Models
+const User = require("../models/User");
+const UserInfo = require("../models/UserInfo");
+
+mongoose.connect('mongodb://localhost:27017/creaftdb', {useNewUrlParser:true});
+
+var db = mongoose.connection;
+
+db.once('open', function(){
+    console.log('Database connection established')
+    
+    User.find({ username: 'nixramirez'}, function (error, documents){
+        console.log(documents)
+    })
+}); 
