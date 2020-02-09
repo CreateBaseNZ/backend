9185bcfba7226fe4800914859268c8616fa58a70
Louis <clin750@aucklanduni.ac.nz>
2020-02-09 22:34:26 +1300

    Modulated SCSS and started navbar

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -18,23 +18,18 @@ function textSequence(i) {
     // Cycle through words
     let words = ['Creator', 'Maverick', 'Trailblazer', 'Innovator'];
 
-    // if (words.length > i) {
-        document.getElementById("change-text").innerHTML = words[i];
-        document.getElementById("change-text").setAttribute("data-text", words[i]);
+    document.getElementById("change-text").innerHTML = words[i];
+    document.getElementById("change-text").setAttribute("data-text", words[i]);
+    setTimeout(function() {
+        document.getElementById("change-text").classList.remove("glitch");
         setTimeout(function() {
-            document.getElementById("change-text").classList.remove("glitch");
+            document.getElementById("change-text").classList.add("glitch");
             setTimeout(function() {
-                document.getElementById("change-text").classList.add("glitch");
-                setTimeout(function() {
-                    i = (Math.floor(Math.random()*4));
-                    textSequence(i);
-                    console.log(i);
-                }, (100 + Math.random()*100));
-            }, (500 + Math.random()*1500));
-        }, (50 + Math.random()*50));
-    // } else if (words.length == i) { // Loop
-    //     textSequence(0);
-    // }
+                i = (Math.floor(Math.random()*4));
+                textSequence(i);
+            }, (100 + Math.random()*100));
+        }, (500 + Math.random()*1500));
+    }, (50 + Math.random()*50));
 }
 
 /*=================================================================================
