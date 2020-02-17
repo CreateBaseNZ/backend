9664099a58ad5dc55086579186314fa41dea80cf
Louis <clin750@aucklanduni.ac.nz>
2020-02-17 23:06:35 +1300

    Home page drafting styles

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -2,11 +2,14 @@ const navInit = () => {
   // Set variable names
   const navElement = document.querySelector(".nav-sidebar");
   const hamburgerElement = document.querySelector(".hamburger");
+  const darkFilter = document.querySelector(".nav-darken-filter");
 
   navElement.addEventListener("mouseenter", function() {
     hamburgerElement.classList.toggle("is-active");
+    darkFilter.style.opacity = "0.05";
   });
   navElement.addEventListener("mouseleave", function() {
     hamburgerElement.classList.toggle("is-active");
+    darkFilter.style.opacity = "0";
   });
 }
