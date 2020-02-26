ef2ec1e9b2b5266cbf59c71d1bd5add8eab45e6f
Louis <clin750@aucklanduni.ac.nz>
2020-02-26 13:12:56 +1300

    Almost completed eng kit on homepage

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -37,18 +37,36 @@ const homeInit = () => {
   const stcSlide = document.querySelector('.eng-kit-stc');
   
   brycepButton.addEventListener("click", function() {
+    brycepButton.classList.add('eng-kit-btn-focus');
+    if (hyelosButton.classList.contains('eng-kit-btn-focus')) {
+      hyelosButton.classList.remove('eng-kit-btn-focus');
+    } else if (stcButton.classList.contains('eng-kit-btn-focus')) {
+      stcButton.classList.remove('eng-kit-btn-focus');
+    }
     brycepSlide.style.display = "flex";
     hyelosSlide.style.display = "none";
     stcSlide.style.display = "none";
   });
 
   hyelosButton.addEventListener("click", function() {
+    if (brycepButton.classList.contains('eng-kit-btn-focus')) {
+      brycepButton.classList.remove('eng-kit-btn-focus');
+    } else if (stcButton.classList.contains('eng-kit-btn-focus')) {
+      stcButton.classList.remove('eng-kit-btn-focus');
+    }
+    hyelosButton.classList.add('eng-kit-btn-focus');
     brycepSlide.style.display = "none";
     hyelosSlide.style.display = "flex";
     stcSlide.style.display = "none";
   });
 
   stcButton.addEventListener("click", function() {
+    if (brycepButton.classList.contains('eng-kit-btn-focus')) {
+      brycepButton.classList.remove('eng-kit-btn-focus');
+    } else if (hyelosButton.classList.contains('eng-kit-btn-focus')) {
+      hyelosButton.classList.remove('eng-kit-btn-focus');
+    }
+    stcButton.classList.add('eng-kit-btn-focus');
     brycepSlide.style.display = "none";
     hyelosSlide.style.display = "none";
     stcSlide.style.display = "flex";
