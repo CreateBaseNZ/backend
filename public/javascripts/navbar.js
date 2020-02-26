aa43a3f85e9c5fd27016cb49280bd43b006d1280
Louis <clin750@aucklanduni.ac.nz>
2020-02-27 01:13:26 +1300

    Redesigned landing section on home page

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -3,13 +3,13 @@ const navInit = () => {
   const navElement = document.querySelector(".nav-sidebar");
   const hamburgerElement = document.querySelector(".hamburger");
   const darkFilter = document.querySelector(".nav-darken-filter");
-
+    
   navElement.addEventListener("mouseenter", function() {
-    hamburgerElement.classList.toggle("is-active");
+    hamburgerElement.classList.add("is-active");
     darkFilter.style.opacity = "0.1";
   });
   navElement.addEventListener("mouseleave", function() {
-    hamburgerElement.classList.toggle("is-active");
+    hamburgerElement.classList.remove("is-active");
     darkFilter.style.opacity = "0";
   });
 }
