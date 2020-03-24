d300d3768a6c4e605c0250e3115c85c476f91b6d
Louis <clin750@aucklanduni.ac.nz>
2020-03-24 15:36:54 +1300

    Team page desktop completed

--- public/javascripts/team.js
+++ public/javascripts/team.js
@@ -16,59 +16,42 @@ const teamInit = async() => {
     var pos = this.scrollY / document.documentElement.clientHeight
     console.log(pos)
 
-    if (pos > 1.85) {
-      team[8][3].style.display = 'block'
-      team[8][5].style.display = 'block'
-
-      team[0][3].style.display = 'none'
-      team[0][5].style.display = 'none'
-      team[2][3].style.display = 'none'
-      team[2][5].style.display = 'none'
-      team[1][3].style.display = 'none'
-      team[1][5].style.display = 'none'
-      team[3][3].style.display = 'none'
-      team[3][5].style.display = 'none'
-    } else if (pos > 1.4) {
-      team[5][3].style.display = 'block'
-      team[5][5].style.display = 'block'
-      team[7][3].style.display = 'block'
-      team[7][5].style.display = 'block'
-    } else if (pos > 1.2) {
-      team[4][3].style.display = 'block'
-      team[4][5].style.display = 'block'
+    if (pos > 1.65) {
       team[6][3].style.display = 'block'
       team[6][5].style.display = 'block'
-
-      team[8][3].style.display = 'none'
-      team[8][5].style.display = 'none'
-    } else if (pos > 0.7) {
-      team[1][3].style.display = 'block'
-      team[1][5].style.display = 'block'
+      team[6][7].style.display = 'block'
+      team[7][3].style.display = 'block'
+      team[7][5].style.display = 'block'
+      team[7][7].style.display = 'block'
+      team[8][3].style.display = 'block'
+      team[8][5].style.display = 'block'
+      team[8][7].style.display = 'block'
+    } else if (pos > 1.1) {
       team[3][3].style.display = 'block'
       team[3][5].style.display = 'block'
-    } else if (pos > 0.5) {
+      team[3][7].style.display = 'block'
+      team[4][3].style.display = 'block'
+      team[4][5].style.display = 'block'
+      team[4][7].style.display = 'block'
+      team[5][3].style.display = 'block'
+      team[5][5].style.display = 'block'
+      team[5][7].style.display = 'block'
+    } else if (pos > 0.55) {
       team[0][3].style.display = 'block'
       team[0][5].style.display = 'block'
+      team[0][7].style.display = 'block'
+      team[1][3].style.display = 'block'
+      team[1][5].style.display = 'block'
+      team[1][7].style.display = 'block'
       team[2][3].style.display = 'block'
       team[2][5].style.display = 'block'
-
-      team[4][3].style.display = 'none'
-      team[4][5].style.display = 'none'
-      team[6][3].style.display = 'none'
-      team[6][5].style.display = 'none'
-      team[5][3].style.display = 'none'
-      team[5][5].style.display = 'none'
-      team[7][3].style.display = 'none'
-      team[7][5].style.display = 'none'
+      team[2][7].style.display = 'block'
     } else if (pos == 0) {
-      team[0][3].style.display = 'none'
-      team[0][5].style.display = 'none'
-      team[2][3].style.display = 'none'
-      team[2][5].style.display = 'none'
-      team[1][3].style.display = 'none'
-      team[1][5].style.display = 'none'
-      team[3][3].style.display = 'none'
-      team[3][5].style.display = 'none'
+      for(var i = 0; i < num_membs; i++){
+        team[i][3].style.display = 'none'
+        team[i][5].style.display = 'none'
+        team[i][7].style.display = 'none'
+      }
     }
   });
 
