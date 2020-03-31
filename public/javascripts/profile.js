fbf1bfafef938d6d4c14129d516bf7fc62d7b244
Louis <clin750@aucklanduni.ac.nz>
2020-04-01 11:09:02 +1300

    Profile receives user input for name, location, and bio

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,12 +1,12 @@
-
-
 const profileInit = async() => {
 
+  // [TO DO] Get values from server
   let name, location, bio
   name = 'Louii'
   location = 'auckland, new zealand'
   bio = 'This is a temporary bio that may be up to 250 characters long, but the final numbers can be longer or shorter. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est veritatis magnam ipsa unde illo cumque quod quas magni nobis, voluptatem nece.'
 
+  // Update all markup (display + edit)
   document.querySelector('.profile-name').innerHTML = name
   document.querySelector('.profile-location').innerHTML = location
   document.querySelector('.profile-bio').innerHTML = bio
@@ -22,18 +22,33 @@ const profileInit = async() => {
     section.classList.toggle('my-profile-section-edit')
   })
   saveBtn.addEventListener('click', () => {
+    // Change the displayed text
     section.classList.toggle('my-profile-section-edit')
+
+    // Save new variables
+    name = document.querySelector('.profile-name').innerHTML 
+    location = document.querySelector('.profile-location').innerHTML
+    bio = document.querySelector('.profile-bio').innerHTML
+
+    // [TO DO] Post to server
+
   })
   cancelBtn.addEventListener('click', () => {
     section.classList.toggle('my-profile-section-edit')
+
+    // Revert all changes back to variables
+    document.querySelector('.profile-name').innerHTML = name
+    document.querySelector('.profile-location').innerHTML = location
+    document.querySelector('.profile-bio').innerHTML = bio
   })
 }
 
+// Previews uploaded profile picture
 var loadFile = function(event) {
   var output = document.getElementById('profile-preview')
-  output.src = URL.createObjectURL(event.target.files[0]);
+  output.src = URL.createObjectURL(event.target.files[0])
   output.onload = function() {
     URL.revokeObjectURL(output.src) // free memory
   }
   console.log('yes')
-};
+}
