4fb433f10db0603de0485c21ecc71552a82db05e
byouu <bradyoung109@gmail.com>
2020-04-07 15:51:30 +1200

    3D info mobile swiper completed

---
+++ public/javascripts/printing.js
@@ -0,0 +1,12 @@
+const printingInit = async() => {
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
