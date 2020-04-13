06271f93faaf9820d1f51f6a0fbe955ded799a42
byouu <bradyoung109@gmail.com>
2020-04-13 23:09:02 +1200

    Responsive services section update

--- public/javascripts/printing.js
+++ public/javascripts/printing.js
@@ -1,12 +1,21 @@
 const printingInit = async() => {
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'fraction',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+
+  history.scrollRestoration = "manual"
+  var mq = window.matchMedia("(min-width: 60em)")
+
+  var landscape = window.innerWidth > window.innerHeight
+
+  if (mq.matches && landscape)  {
+  } else {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+  }
 }
