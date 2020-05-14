4b87dd0e31fd24876ff28915f43e2b8cfb08c4dd
byouu <bradyoung109@gmail.com>
2020-05-14 22:55:28 +1200

    Coming soon eng kits

--- public/javascripts/engkits.js
+++ public/javascripts/engkits.js
@@ -1,29 +1,22 @@
-const loginInit = () => {
-    textSequence(0, ['Creator', 'Maverick', 'Trailblazer', 'Innovator'])
-}
-
 const engKitsInit = () => {
     textSequence(0, ['COMING SOON', 'ENG KITS'])
 }
 
-const toggleContainer = () => {
-    container.classList.toggle("right-panel-active")
-}
-
 function textSequence(i, words) {
     // Cycle through words
-    // let words = ['Creator', 'Maverick', 'Trailblazer', 'Innovator']
-    console.log(words)
     document.getElementById("change-text").innerHTML = words[i]
+    document.getElementById("change-text").setAttribute('data-text', words[i])
     setTimeout(function() {
         document.getElementById("change-text").classList.remove("glitch")
         setTimeout(function() {
             document.getElementById("change-text").classList.add("glitch")
             setTimeout(function() {
-                i = (Math.floor(Math.random()*words.length))
+                i += 1
+                if (i >= words.length){
+                    i = 0
+                }
                 textSequence(i,words)
             }, (100 + Math.random()*100))
         }, (500 + Math.random()*1500))
     }, (50 + Math.random()*50))
-
 }
