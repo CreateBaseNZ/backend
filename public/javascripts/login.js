7176ba482e7d2efb56e36d3370c2429bd3c61aa8
byouu <bradyoung109@gmail.com>
2020-05-14 22:06:18 +1200

    Auto stash before merge of "master" and "origin/master"

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,17 +13,17 @@ const toggleContainer = () => {
 function textSequence(i, words) {
     // Cycle through words
     // let words = ['Creator', 'Maverick', 'Trailblazer', 'Innovator']
-
-    document.getElementById("change-text").innerHTML = words[i];
+    console.log(words)
     document.getElementById("change-text").setAttribute("data-text", words[i])
     setTimeout(function() {
         document.getElementById("change-text").classList.remove("glitch")
         setTimeout(function() {
             document.getElementById("change-text").classList.add("glitch")
             setTimeout(function() {
-                i = (Math.floor(Math.random()*4))
+                i = (Math.floor(Math.random()*words.length))
                 textSequence(i)
             }, (100 + Math.random()*100))
         }, (500 + Math.random()*1500))
     }, (50 + Math.random()*50))
+
 }
