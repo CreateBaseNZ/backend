30cfc5dd6d75170c99cc732868651ed28da906b2
byouu <bradyoung109@gmail.com>
2020-05-21 22:23:54 +1200

    Hide/fade subscribe notif

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -131,6 +131,53 @@ const unsubscribe = (input) => {
 
 //   return;
 // }
+/*=========================================================================================
+Global notifs
+=========================================================================================*/
+
+function  subscribeNotif() {
+  //Create div to insert
+  let newDiv = document.createElement('div')
+  newDiv.className = 'subbed-notif'
+  let messageWrap = document.createElement('div')
+  newDiv.appendChild(messageWrap).className = 'subMsgWrap'
+  messageWrap.appendChild(document.createElement('i')).className = 'fab fa-telegram-plane'
+  messageWrap.appendChild(document.createElement('p')).innerHTML = 'Thank you for subscribing to the newsletter!'
+
+  //Find location to insert div
+  let notifDiv = document.getElementById('notification-wrap')
+  let cookieDiv = document.getElementById('cookie-container')
+
+  //Insert div
+  notifDiv.insertBefore(newDiv, cookieDiv.nextSibling)
+
+  // Fade out
+  setTimeout(() => {
+      newDiv.style.transition = 'all 2s';
+      newDiv.style.opacity = 0;
+      // Hide
+      setTimeout(() => {
+          newDiv.style.display = 'none';
+      }, 1000);
+  }, 3000);
+}
+
+subscribe.listener = async () => {
+  // Declare and initialise variables
+  let input = document.getElementById('sign-up-eml');
+  let subBtn = document.getElementById('subscribe-main');
+  // Subscribe user
+  try {
+      await subscribe(input.value);
+  } catch (error) {
+      return console.log(error);
+  }
+  input.value = ''; // Clear email input field
+  // Success Handler
+  subBtn.innerHTML = 'SUBSCRIBE NEW EMAIL';
+  subscribeNotif();
+  return;
+}
 
 /*=========================================================================================
 END
--- public/javascripts/market.js
+++ public/javascripts/market.js
@@ -21,38 +21,3 @@ function textSequence(i, words) {
     }, (50 + Math.random() * 50))
 
 }
-
-function subscribeNotif() {
-    //Create div to insert
-    let newDiv = document.createElement('div')
-    newDiv.className = 'subbed-notif'
-    let messageWrap = document.createElement('div')
-    newDiv.appendChild(messageWrap).className = 'subMsgWrap'
-    messageWrap.appendChild(document.createElement('i')).className = 'fab fa-telegram-plane'
-    messageWrap.appendChild(document.createElement('p')).innerHTML = 'Thank you for subscribing to the newsletter!'
-
-    //Find location to insert div
-    let notifDiv = document.getElementById('notification-wrap')
-    let cookieDiv = document.getElementById('cookie-container')
-
-    //Insert div
-    notifDiv.insertBefore(newDiv, cookieDiv.nextSibling)
-}
-
-subscribe.listener = async () => {
-    // Declare and initialise variables
-    let input = document.getElementById('sign-up-eml');
-    let subBtn = document.getElementById('subscribe-main');
-    // Subscribe user
-    try {
-        await subscribe(input.value);
-    } catch (error) {
-        return console.log(error);
-    }
-    input.value = ''; // Clear email input field
-    // Success Handler
-    subBtn.innerHTML = 'SUBSCRIBE NEW EMAIL';
-    subscribeNotif();
-    return;
-}
-
