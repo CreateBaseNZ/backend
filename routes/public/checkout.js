5922914f16d979dcf383dbaac1dc8748f168232e
Carl Velasco <carlvelasco96@gmail.com>
2020-05-24 23:02:33 +1200

    Voucher System Completed

--- routes/public/checkout.js
+++ routes/public/checkout.js
@@ -21,6 +21,7 @@ MODELS
 
 const Account = require("../../model/Account.js");
 const Customer = require("../../model/Customer.js");
+const Discount = require("../../model/Discount.js");
 const Order = require("../../model/Order.js");
 const Make = require("../../model/Make.js");
 
@@ -277,15 +278,15 @@ router.get("/checkout/bank-transfer", restrictedPages, async (req, res) => {
   // BUILD THE ORDER
   let order;
   // Create the find object
-  let object;
+  let query;
   if (accountId) {
-    object = { accountId, status: "created" };
+    query = { accountId, status: "created" };
   } else {
-    object = { sessionId, status: "created" };
+    query = { sessionId, status: "created" };
   }
   // Fetch existing active order
   try {
-    order = await Order.transaction(object);
+    order = await Order.transaction(query);
   } catch (error) {
     return res.send({ status: "failed", content: error });
   }
@@ -386,6 +387,72 @@ router.post("/checkout/make/update", restrictedPages, async (req, res) => {
   }
   // RETURN SUCCESS
   return res.send({ status: "success", content: amount });
+});
+
+// @route     POST /checkout/discount/add
+// @desc      
+// @access    Private
+router.post("/checkout/discount/add", restrictedPages, async (req, res) => {
+  // FETCH DISCOUNT WITH THE SAID CODE
+  const account = req.user;
+  const code = req.body;
+  // Fetch Discount
+  let discount;
+  try {
+    discount = await Discount.findOne({ code });
+  } catch (error) {
+    return res.send({ status: "failed", content: error });
+  }
+  // Validate the discount
+  if (!discount) return res.send({ status: "failed", content: "no discount" });
+  // Duration
+  const startMoment = moment(discount.duration.start);
+  switch (discount.duration.type) {
+    case "unlimited":
+      if (todayMoment.diff(startMoment) >= 0) break;
+      return res.send({ status: "failed", content: "discount is inactive" });
+    case "limited":
+      const endMoment = moment(discount.duration.end);
+      if ((todayMoment.diff(startMoment) >= 0) && (endMoment.diff(todayMoment) >= 0)) break;
+      return res.send({ status: "failed", content: "discount is inactive" });
+    default:
+      return res.send({ status: "failed", content: "discount is inactive" });
+  }
+  // Audience
+  switch (discount.audience.type) {
+    case "global":
+      break;
+    case "account":
+      if (discount.audience.accounts.indexOf(account.type) !== -1) break;
+      return res.send({ status: "failed", content: "invalid account type" });
+    case "customer":
+      if (discount.audience.customers.indexOf(account._id) !== -1) break;
+      return res.send({ status: "failed", content: "invalid account type" });
+    default:
+      return res.send({ status: "failed", content: "invalid account type" });
+  }
+  // FETCH THE USERS ORDERS
+  let orders;
+  try {
+    orders = await Order.find({ accountId: account._id });
+  } catch (error) {
+    return res.send({ status: "failed", content: "no orders" });
+  }
+  // Order-based validation
+  const activeOrder = (orders.filter(order => (order.status === "created")))[0];
+  if (!activeOrder) return res.send({ status: "failed", content: "no active order" });
+  if (activeOrder.discounts.indexOf(discount._id) !== -1) {
+    return res.send({ status: "failed", content: "discount already exist" });
+  }
+  orders = orders.filter(order => {
+    if (order.discounts.indexOf(discount._id) !== -1) return true;
+    return false;
+  });
+  if (discount.usage.limit <= orders.length) {
+    return res.send({ status: "failed", content: "discount limit reached" });
+  }
+  // SUCCESS RESPONSE
+  return res.send({ status: "failed", content: discount });
 });
 
 /*-----------------------------------------------------------------------------------------
