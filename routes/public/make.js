056c714ae5f054709fcb1079e18be586c3c97180
Carl Velasco <carlvelasco96@gmail.com>
2020-06-05 12:40:16 +1200

    Project Page: Thumbnail Upload Backend System Completed

--- routes/public/make.js
+++ routes/public/make.js
@@ -51,76 +51,72 @@ const upload = require("../../config/upload.js");
 ROUTES
 =========================================================================================*/
 
-router.post(
-  "/make/submit",
-  upload.single("file"),
-  verifiedAccess,
-  async (req, res) => {
-    const accountId = mongoose.Types.ObjectId(req.user._id);
-    const file = {
-      id: mongoose.Types.ObjectId(req.file.id),
-      name: req.file.filename,
-    };
-    const build = req.body.build;
-    const quick = req.body.quick;
-    const process = req.body.material.split("-")[0];
-    const material = req.body.material.split("-")[1];
-    const quality = req.body.quality;
-    const strength = req.body.strength;
-    const colour = req.body.colour;
-    const quantity = req.body.quantity;
-    const message = req.body.note;
+router.post("/make/submit", upload.single("file"), verifiedAccess, async (req, res) => {
+  const accountId = mongoose.Types.ObjectId(req.user._id);
+  const file = {
+    id: mongoose.Types.ObjectId(req.file.id),
+    name: req.file.filename,
+  };
+  const build = req.body.build;
+  const quick = req.body.quick;
+  const process = req.body.material.split("-")[0];
+  const material = req.body.material.split("-")[1];
+  const quality = req.body.quality;
+  const strength = req.body.strength;
+  const colour = req.body.colour;
+  const quantity = req.body.quantity;
+  const message = req.body.note;
 
-    // Create a make object
-    let make = new Make({
+  // Create a make object
+  let make = new Make({
+    accountId,
+    file,
+    build,
+    quick,
+    process,
+    material,
+    quality,
+    strength,
+    colour,
+    quantity,
+  });
+
+  // Set status
+  make.updateStatus("awaitingQuote");
+  // Create a comment
+  if (message) {
+    let comment = new Comment({
       accountId,
-      file,
-      build,
-      quick,
-      process,
-      material,
-      quality,
-      strength,
-      colour,
-      quantity,
+      message,
     });
 
-    // Set status
-    make.updateStatus("awaitingQuote");
-    // Create a comment
-    if (message) {
-      let comment = new Comment({
-        accountId,
-        message,
-      });
-
-      try {
-        await comment.setDate();
-      } catch (error) {
-        return res.send({ status: "failed", data: error });
-      }
-
-      let savedComment;
-
-      try {
-        savedComment = await comment.save();
-      } catch (error) {
-        return res.send({ status: "failed", data: error });
-      }
-
-      make.comment = mongoose.Types.ObjectId(savedComment._id);
-    }
-    // Save make
-    let savedMake;
-
     try {
-      savedMake = await make.save();
+      await comment.setDate();
     } catch (error) {
       return res.send({ status: "failed", data: error });
     }
 
-    return res.send({ status: "success", data: savedMake });
+    let savedComment;
+
+    try {
+      savedComment = await comment.save();
+    } catch (error) {
+      return res.send({ status: "failed", data: error });
+    }
+
+    make.comment = mongoose.Types.ObjectId(savedComment._id);
   }
+  // Save make
+  let savedMake;
+
+  try {
+    savedMake = await make.save();
+  } catch (error) {
+    return res.send({ status: "failed", data: error });
+  }
+
+  return res.send({ status: "success", data: savedMake });
+}
 );
 
 router.get("/profile/customer/fetch/makes", verifiedAccess, async (req, res) => {
