33792adbb3491322131652288ca658830b0460d6
Carl Velasco <carlvelasco96@gmail.com>
2020-06-28 17:48:00 +1200

    Make Page: STL Viewer Implemented

--- public/javascripts/make.js
+++ public/javascripts/make.js
@@ -153,7 +153,9 @@ let make = {
     },
     show: undefined
   },
-  changePage: undefined
+  changePage: undefined,
+  viewerProcess: undefined,
+  viewerUpload: undefined
 };
 
 /*=========================================================================================
@@ -1271,6 +1273,78 @@ make.redirect = () => {
     <a class="rmv-link-css" href="/checkout">Go to Cart</a>
   </div>`;
 };
+
+/* ----------------------------------------------------------------------------------------
+VIEWER
+---------------------------------------------------------------------------------------- */
+
+make.viewerProcess = (camera, renderer, controls, scene, geometry) => {
+  var material = new THREE.MeshPhongMaterial({
+    color: 0xf0f0f0,
+    specular: 0xf8f8f8,
+    shininess: 0,
+    reflectivity: 0
+  });
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  var middle = new THREE.Vector3();
+  geometry.computeBoundingBox();
+  geometry.boundingBox.getCenter(middle);
+  mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(
+    -middle.x, -middle.y, -middle.z));
+  mesh.geometry.rotateY(-Math.PI / 5);
+
+  var largestDimension = Math.max(geometry.boundingBox.max.x,
+    geometry.boundingBox.max.y,
+    geometry.boundingBox.max.z)
+  camera.position.z = largestDimension * 2.5;
+  var animate = function () {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  };
+  animate();
+}
+
+make.viewerUpload = (event) => {
+  const elementID = "make-viewer";
+  document.querySelector(`#${elementID}`).innerHTML = "";
+  var file = event.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    var elem = document.getElementById(elementID);
+
+    var camera = new THREE.PerspectiveCamera(70, elem.clientWidth / elem.clientHeight, 1, 1000);
+
+    var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(elem.clientWidth, elem.clientHeight);
+    elem.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', function () {
+      renderer.setSize(elem.clientWidth, elem.clientHeight);
+      camera.aspect = elem.clientWidth / elem.clientHeight;
+      camera.updateProjectionMatrix();
+    }, false);
+
+    /* Next, we’ll configure the controls using OrbitControls. */
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.rotateSpeed = 0.4;
+    controls.dampingFactor = 0.1;
+    controls.enableZoom = false;
+    // controls.autoRotate = true;
+    // controls.autoRotateSpeed = .75;
+
+    var scene = new THREE.Scene();
+    scene.add(new THREE.HemisphereLight(0xffffff, 0x787878, 0.8));
+
+    const loader = new THREE.STLLoader();
+    let geometry = loader.parse(e.target.result);
+    make.viewerProcess(camera, renderer, controls, scene, geometry);
+  };
+  reader.readAsBinaryString(file);
+}
 
 /*=========================================================================================
 END
