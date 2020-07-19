f763e2aec1751617b6edf840e14d0b3b8c6f9279
Louis <clin750@aucklanduni.ac.nz>
2020-07-19 18:03:21 +1200

    Mobile projects completed

--- public/javascripts/profile/projects.js
+++ public/javascripts/profile/projects.js
@@ -67,26 +67,27 @@ projects.declareVariables = () => {
 
 projects.eventListeners = () => {
   const mq = window.matchMedia("(min-width: 850px)")
-  if (mq.matches) {
-    let fav = document.getElementById('proj-fav-container')
-    fav.addEventListener('wheel', function (e) {
-      if (e.deltaY > 0) {
-        fav.scrollLeft += 100
-      } else {
-        fav.scrollLeft -= 100
-      }
-      e.preventDefault()
-    })
-    let all = document.getElementById('proj-all-container')
-    all.addEventListener('wheel', function (e) {
+  
+  let fav = document.getElementById('proj-fav-container')
+  fav.addEventListener('wheel', function (e) {
+    if (e.deltaY > 0) {
+      fav.scrollLeft += 100
+    } else {
+      fav.scrollLeft -= 100
+    }
+    e.preventDefault()
+  })
+  let all = document.getElementById('proj-all-container')
+  all.addEventListener('wheel', function (e) {
+    if (mq.matches) {
       if (e.deltaY > 0) {
         all.scrollLeft += 100
       } else {
         all.scrollLeft -= 100
       }
       e.preventDefault()
-    })
-  }
+    }
+  })
 
   document.getElementById('proj-add').addEventListener('click', () => {
     projects.showProjPop()
@@ -386,7 +387,7 @@ projects.renderProjPop = (proj, allMakes) => {
   left.appendChild(imgOverlay)
   let edit = document.createElement('div')
   edit.className = 'proj-pop-img-edit'
-  edit.innerHTML = 'Click anywhere to edit'
+  edit.innerHTML = 'Click anywhere on the image to edit'
   left.appendChild(edit)
   // mid container
   let mid = document.createElement('div')
@@ -410,6 +411,7 @@ projects.renderProjPop = (proj, allMakes) => {
   nameInput.placeholder = 'Give your project a name'
   mid.appendChild(nameInput)
   let makes = document.createElement('h3')
+  makes.className = 'proj-pop-makes-header'
   makes.innerHTML = 'Makes'
   mid.appendChild(makes)
   let makesContainer = document.createElement('div')
@@ -433,17 +435,17 @@ projects.renderProjPop = (proj, allMakes) => {
     projects.trash(proj.id)
   })
   btnContainer.appendChild(del)
-  let save = document.createElement('button')
-  save.className = 'grad-btn proj-pop-save'
-  save.innerHTML = 'Save'
-  save.addEventListener('click', (e) => {
+  let saveWeb = document.createElement('button')
+  saveWeb.className = 'grad-btn proj-pop-save'
+  saveWeb.innerHTML = 'Save'
+  saveWeb.addEventListener('click', (e) => {
     if (e.target.parentElement.parentElement.parentElement.parentElement.id === 'new-proj-pop-wrapper') {
       projects.saveNew(allMakes)
     } else {
       projects.saveExisting(proj.id, allMakes)
     }
   })
-  btnContainer.appendChild(save)
+  btnContainer.appendChild(saveWeb)
   // right container
   let right = document.createElement('div')
   right.className = 'proj-pop-right'
@@ -460,6 +462,27 @@ projects.renderProjPop = (proj, allMakes) => {
   let temp2 = document.createElement('p')
   temp2.innerHTML = 'You can add Makes to this project later'
   right.appendChild(temp2)
+  // mobile save and delete buttons
+  let btnContainerMob = document.createElement('div')
+  btnContainerMob.className = 'proj-pop-btn-container'
+  container.appendChild(btnContainerMob)
+  let delMob = document.createElement('i')
+  delMob.className = 'far fa-trash-alt proj-pop-delete'
+  delMob.addEventListener('click', () => {
+    projects.trash(proj.id)
+  })
+  btnContainerMob.appendChild(delMob)
+  let saveMob = document.createElement('button')
+  saveMob.className = 'grad-btn proj-pop-save'
+  saveMob.innerHTML = 'Save'
+  saveMob.addEventListener('click', (e) => {
+    if (e.target.parentElement.parentElement.parentElement.id === 'new-proj-pop-wrapper') {
+      projects.saveNew(allMakes)
+    } else {
+      projects.saveExisting(proj.id, allMakes)
+    }
+  })
+  btnContainerMob.appendChild(saveMob)
 
   if (proj) {
     wrapper.id = proj.id + '-proj-pop-wrapper'
@@ -485,7 +508,9 @@ projects.renderProjPop = (proj, allMakes) => {
 
 projects.hideProjPop = (status, condition, projID) => {
   // transition screens
-  document.getElementById('proj-card-wrapper').style.top = '0'
+  if (window.matchMedia("(min-width: 850px)").matches) {
+    document.getElementById('proj-card-wrapper').style.top = '0'
+  }
   // not deleting
   if (projID) {
     document.getElementById(projID + '-proj-pop-wrapper').style.top = '100%'
@@ -498,7 +523,9 @@ projects.hideProjPop = (status, condition, projID) => {
 
 projects.showProjPop = (projID) => {
   // transition screens
-  document.getElementById('proj-card-wrapper').style.top = '-100%'
+  if (window.matchMedia("(min-width: 850px)").matches) {
+    document.getElementById('proj-card-wrapper').style.top = '-100%'
+  }
   if (projID) {
     document.getElementById(projID + '-proj-pop-wrapper').style.top = '0'
   } else {
