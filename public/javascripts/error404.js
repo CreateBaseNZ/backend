a07a1cc4d6a5698c3e5b4829655e7017027dc3dd
Louis <clin750@aucklanduni.ac.nz>
2020-09-10 14:29:33 +1200

    404 redesign

--- public/javascripts/error404.js
+++ public/javascripts/error404.js
@@ -7,6 +7,7 @@ let error = {
 
   // FUNCTIONS
   initialise: undefined,
+  glitch: undefined,
   fullHeight: undefined
 }
 
@@ -24,10 +25,28 @@ error.initialise = async () => {
   // REMOVE STARTUP LOADER
   removeLoader();
   error.fullHeight();
+  error.glitch();
 }
 
 error.fullHeight = () => {
-  document.querySelector('.main-page').style.height = document.querySelector('.main-page').offsetHeight - document.querySelector('.footer-section').offsetHeight + 'px'
+  // document.querySelector('.main-page').style.height = document.querySelector('.main-page').offsetHeight - document.querySelector('.footer-section').offsetHeight + 'px'
+}
+
+error.glitch = () => {
+  let el = document.getElementById("error-header")
+  let i = Math.round(Math.random())
+  let words = ['Oops','4oh4']
+  el.innerHTML = words[i]
+  el.setAttribute('data-text', words[i])
+  setTimeout(function () {
+    el.classList.remove("glitch")
+    setTimeout(function () {
+      el.classList.add("glitch")
+      setTimeout(function () {
+        error.glitch(words);
+      }, (100 + Math.random() * 100))
+    }, (500 + Math.random() * 1500))
+  }, (50 + Math.random() * 50))
 }
 
 /* ========================================================================================
