d1abbda514f98b74242dba60bff9dfcf4bd447d0
Louis <clin750@aucklanduni.ac.nz>
2020-09-15 18:07:45 +1200

    For Yoobee

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -34,35 +34,65 @@ story.initialise = async () => {
 }
 
 story.declare = () => {
-  story.elements = [
-    document.getElementById('story-section-1').querySelector('h2'),
-    document.getElementById('story-collab'),
-    document.getElementById('story-section-1').querySelector('.story-text'),
-    document.getElementById('story-section-2').querySelector('h2'),
-    document.getElementById('story-team'),
-    document.getElementById('story-carl'),
-    document.getElementById('story-section-2').querySelector('.story-text'),
-    document.getElementById('story-section-2').querySelector('.story-go-link'),
-    document.getElementById('our-vision'),
-    document.getElementById('story-carlos-craig'),
-    document.getElementById('story-whiteboard'),
-    document.getElementById('our-mission'),
-    document.getElementById('story-web-team'),
-    document.getElementById('story-section-5').querySelector('h2'),
-    document.getElementById('strategy-a'),
-    document.getElementById('story-brydon'),
-    document.getElementById('strategy-b'),
-    document.getElementById('story-section-5').querySelector('.story-go-link'),
-    document.getElementById('story-section-7').querySelector('h2'),
-    document.getElementById('story-kit-team'),
-    document.getElementById('story-velocity'),
-    document.getElementById('story-laptop'),
-    document.getElementById('story-robotic-arm'),
-    document.getElementById('story-section-8').querySelector('.story-go-link'),
-    document.getElementById('story-section-9').querySelector('h2'),
-    document.getElementById('story-social-container'),
-
-  ]
+  if (window.matchMedia("(min-width: 850px)").matches) {
+    story.elements = [
+      document.getElementById('story-section-1').querySelector('h2'),
+      document.getElementById('story-collab'),
+      document.getElementById('story-section-1').querySelector('.story-text'),
+      document.getElementById('story-section-2').querySelector('h2'),
+      document.getElementById('story-team'),
+      document.getElementById('story-carl'),
+      document.getElementById('story-section-2').querySelector('.story-text'),
+      document.getElementById('story-section-2').querySelector('.story-go-link'),
+      document.getElementById('our-vision'),
+      document.getElementById('story-carlos-craig'),
+      document.getElementById('story-whiteboard'),
+      document.getElementById('our-mission'),
+      document.getElementById('story-section-4').querySelector('h2'),
+      document.getElementById('strategy-a'),
+      document.getElementById('story-brydon'),
+      document.getElementById('strategy-b'),
+      document.getElementById('story-section-4').querySelector('.story-go-link'),
+      document.getElementById('story-section-5').querySelector('h2'),
+      document.getElementById('story-kit-team'),
+      document.getElementById('story-velocity'),
+      document.getElementById('story-laptop'),
+      document.getElementById('story-robotic-arm'),
+      document.getElementById('story-section-5').querySelector('.story-go-link'),
+      document.getElementById('story-section-6').querySelector('h2'),
+      document.getElementById('story-web-team'),
+      document.getElementById('story-social-container')
+    ]
+  } else {
+    story.elements = [
+      document.getElementById('story-section-1').querySelector('h2'),
+      document.getElementById('story-collab'),
+      document.getElementById('story-section-1').querySelector('.story-text'),
+      document.getElementById('story-section-2').querySelector('h2'),
+      document.getElementById('story-team'),
+      document.getElementById('story-carl'),
+      document.getElementById('story-section-2').querySelector('.story-text'),
+      document.getElementById('story-section-2').querySelector('.story-go-link'),
+      document.getElementById('our-vision'),
+      document.getElementById('story-carlos-craig'),
+      document.getElementById('story-whiteboard'),
+      document.getElementById('our-mission'),
+      document.getElementById('story-section-4').querySelector('h2'),
+      document.getElementById('strategy-a'),
+      document.getElementById('story-brydon'),
+      document.getElementById('strategy-b'),
+      document.getElementById('story-section-4').querySelector('.story-go-link'),
+      document.getElementById('story-section-5').querySelector('h2'),
+      document.getElementById('story-kit-team'),
+      document.getElementById('story-velocity'),
+      document.getElementById('story-laptop'),
+      document.getElementById('story-robotic-arm'),
+      document.getElementById('story-section-5').querySelector('.story-go-link'),
+      document.getElementById('story-section-6').querySelector('h2'),
+      document.getElementById('story-web-team'),
+      document.getElementById('story-social-container')
+    ]
+  }
   story.elements.forEach(function(el, i) {
     story.positions.push(el.getBoundingClientRect().top)
   })
