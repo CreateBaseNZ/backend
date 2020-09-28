9665771761bb4cc9d6926c631a8b14a60a2fbb98
Carl Velasco <carlvelasco96@gmail.com>
2020-09-29 09:53:40 +1300

    test

--- routes/contact-us.js
+++ routes/contact-us.js
@@ -45,11 +45,13 @@ router.post("/contact-us/submit-inquiry", async (req, res) => {
     return res.send(data);
   }
   // SEND NOTIFICATION
+  /*
   try {
     await message.sendInquiryEmailNotification();
   } catch (error) {
     return res.send(data);
   }
+  */
   // SUCCESS HANDLER
   return res.send({ status: "succeeded", content: "Inquiry has been sent" });
 });
