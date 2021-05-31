bba4e3a075f0af3ec8a8629ac22457dc3a48dc30
Carl Velasco <carlvelasco96@gmail.com>
2021-06-01 00:17:17 +1200

    Added cors to enable cross platform communication

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
+const cors = require("cors");
 
 /*=========================================================================================
 VARIABLES
@@ -56,6 +57,7 @@ app.use((req, res, next) => {
   res.removeHeader("Server");
   next();
 });
+app.use(cors());
 
 /*=========================================================================================
 ROUTES
