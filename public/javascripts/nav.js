a9a1cf8423423f10baf615259c14f91f27e64f0e
Brad <bradyoung109gmail.com>
2021-09-13 18:18:14 +1200

    Nav logo change for background

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -17,6 +17,8 @@ let nav = {
     ham: document.querySelector('.hamburger'),
     nav: document.querySelector('nav'),
     navLogo: document.querySelector('.nav-logo'),
+    navLogoDark: document.querySelector('.cb-logo-dark'),
+    navLogoLight: document.querySelector('.cb-logo-light'),
     navMenu: document.querySelector('.nav-menu'),
     navItem: document.querySelectorAll('.nav-item'),
     navHamburger: document.querySelector('.hamburger-inner')
@@ -52,6 +54,8 @@ nav.lightNavItem = function(){
   let currentPage = nav.checkPage()
   let navItems = nav.elem.navItem;
   if (currentPage === 'https://createbase.co.nz/' || currentPage === 'https://createbase.co.nz/about' || currentPage === 'https://createbase.co.nz/about#first-section' || currentPage === 'http://127.0.0.1:5501/views/home.html' || currentPage === 'http://127.0.0.1:5501/views/about.html' || currentPage === 'http://localhost/' || currentPage === 'http://localhost/about'|| currentPage === 'http://localhost/about#first-section') {
+    nav.elem.navLogoDark.classList.add('hide');
+    nav.elem.navLogoLight.classList.remove('hide');
     for (let i = 0; i < navItems.length; i++) {
       navItems[i].classList.add('light-menu-text');
     }
@@ -69,12 +73,16 @@ nav.checkTop = function() {
   let navItems = nav.elem.navItem;
   if (window.pageYOffset >= 24) {
     nav.elem.nav.classList.add('sticky')
+    nav.elem.navLogoDark.classList.remove('hide');
+    nav.elem.navLogoLight.classList.add('hide');
     for (let i = 0; i < navItems.length; i++) {
       navItems[i].classList.remove('light-menu-text');
       navItems[i].classList.add('dark-menu-text');
     };
   } else {
     nav.elem.nav.classList.remove('sticky')
+    nav.elem.navLogoLight.classList.remove('hide');
+    nav.elem.navLogoDark.classList.add('hide');
     for (let i = 0; i < navItems.length; i++) {
       navItems[i].classList.add('light-menu-text');
       navItems[i].classList.remove('dark-menu-text');
