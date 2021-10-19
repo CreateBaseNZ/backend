f237f210e3c663560d8426b7c70b6ccfb195e794
Carl Velasco <carlvelasco96@gmail.com>
2021-10-20 10:48:35 +1300

    Progress

--- routes/mail.js
+++ routes/mail.js
@@ -65,7 +65,7 @@ router.post("/mail/subscribe-newsletter", async (req, res) => {
 		return res.send({ status: "error", content: error });
 	}
 	// Success handler
-	return res.send({ status: "succeeded", content: undefined });
+	return res.send({ status: "succeeded" });
 });
 
 // @route   GET /mail/unsubscribe-newsletter/:email
@@ -129,7 +129,7 @@ router.post("/mail/manage-options", async (req, res) => {
 		return res.send({ status: "error", content: error });
 	}
 	// Success handler
-	return res.send({ status: "succeeded", content: undefined });
+	return res.send({ status: "succeeded" });
 });
 
 // @route   POST /mail/retrieve-options
@@ -138,7 +138,7 @@ router.post("/mail/manage-options", async (req, res) => {
 router.post("/mail/retrieve-options", async (req, res) => {
 	// Validate if the PRIVATE_API_KEY match
 	if (req.body.PRIVATE_API_KEY !== process.env.PRIVATE_API_KEY) {
-		return res.send({ status: "critical error", content: undefined });
+		return res.send({ status: "critical error" });
 	}
 	// Fetch the mail instance
 	let mail;
@@ -176,6 +176,7 @@ router.post("/mail/admin/update-cold-emails", async (req, res) => {
 	// https://docs.google.com/spreadsheets/d/1rAkSeFlOyLNeLCr3GPwI7TG8sHc9AOpnF1VMD9Qt1vM/edit?usp=sharing
 	const spreadsheetId = "1rAkSeFlOyLNeLCr3GPwI7TG8sHc9AOpnF1VMD9Qt1vM";
 	// Read rows from spreadsheet
+	// New Zealand
 	const nz = await googleSheets.spreadsheets.values.get({ auth, spreadsheetId, range: "New Zealand" });
 	nz.data.values.shift();
 	for (let i = 0; i < nz.data.values.length; i++) {
@@ -191,7 +192,98 @@ router.post("/mail/admin/update-cold-emails", async (req, res) => {
 			return res.send({ status: "error", content: error });
 		}
 		if (mail) continue;
-		mail = new Mail({ email, notification: { cold: true }, metadata: { name, school, segment, country: "nz" } });
+		mail = new Mail({ email, notification: { cold: true }, metadata: { name, type: "customer-school", school, segment, country: "nz" } });
+		try {
+			await mail.save();
+		} catch (error) {
+			return res.send({ status: "error", content: error });
+		}
+	}
+	// Singapore
+	const sg = await googleSheets.spreadsheets.values.get({ auth, spreadsheetId, range: "Singapore" });
+	sg.data.values.shift();
+	for (let i = 0; i < sg.data.values.length; i++) {
+		const name = sg.data.values[i][0];
+		const email = sg.data.values[i][1];
+		const school = sg.data.values[i][2];
+		const segment = sg.data.values[i][3];
+		// Check if a mail instance with this email exist
+		let mail;
+		try {
+			mail = await Mail.findOne({ email });
+		} catch (error) {
+			return res.send({ status: "error", content: error });
+		}
+		if (mail) continue;
+		mail = new Mail({ email, notification: { cold: true }, metadata: { name, type: "customer-school", school, segment, country: "sg" } });
+		try {
+			await mail.save();
+		} catch (error) {
+			return res.send({ status: "error", content: error });
+		}
+	}
+	// United Kingdom
+	const uk = await googleSheets.spreadsheets.values.get({ auth, spreadsheetId, range: "United Kingdom" });
+	uk.data.values.shift();
+	for (let i = 0; i < uk.data.values.length; i++) {
+		const name = uk.data.values[i][0];
+		const email = uk.data.values[i][1];
+		const school = uk.data.values[i][2];
+		const segment = uk.data.values[i][3];
+		// Check if a mail instance with this email exist
+		let mail;
+		try {
+			mail = await Mail.findOne({ email });
+		} catch (error) {
+			return res.send({ status: "error", content: error });
+		}
+		if (mail) continue;
+		mail = new Mail({ email, notification: { cold: true }, metadata: { name, type: "customer-school", school, segment, country: "uk" } });
+		try {
+			await mail.save();
+		} catch (error) {
+			return res.send({ status: "error", content: error });
+		}
+	}
+	// Success handler
+	return res.send({ status: "succeeded" });
+});
+
+// @route   POST /mail/admin/send-cold-emails
+// @desc
+// @access
+router.post("/mail/admin/send-cold-emails", async (req, res) => {
+	// Validate if the PRIVATE_API_KEY match
+	if (req.body.PRIVATE_API_KEY !== process.env.PRIVATE_API_KEY) {
+		return res.send({ status: "critical error" });
+	}
+	// Validate if the ADMIN_API_KEY match
+	if (req.body.ADMIN_API_KEY !== process.env.ADMIN_API_KEY) {
+		return res.send({ status: "critical error" });
+	}
+	// Fetch mails
+	let mails;
+	try {
+		mails = await Mail.find({ "notification.cold": true, "metadata.type": "customer-school" });
+	} catch (error) {
+		return res.send({ status: "error", content: error });
+	}
+	// Send emails
+	for (let i = 0; i < mails.length; i++) {
+		let mail = mails[i];
+		const options = {
+			recipient: mail.email,
+			receive: `test-${mail.metadata.country}-${mail.metadata.segment}`,
+			notification: "cold",
+			tone: "friendly",
+		};
+		let status;
+		try {
+			status = await mail.sendEmail(options);
+		} catch (data) {
+			return res.send(data);
+		}
+		// Save the updates
 		try {
 			await mail.save();
 		} catch (error) {
