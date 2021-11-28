845dd44d04179d224187b1717cd0ad8d19eedbf9
Carl Velasco <carlvelasco96@gmail.com>
2021-11-28 16:39:46 +1300

    Delete Class Backend Programme

--- algorithms/demolish.js
+++ algorithms/demolish.js
@@ -21,7 +21,7 @@ const Profile = require("../model/Profile.js");
 
 // FUNCTIONS ================================================
 
-demolish.groups = (groups = [], licenses = [], classes = []) => {
+demolish.groups = (groups = [], licenses = [], classes = [], process = "delete group") => {
 	return new Promise(async (resolve, reject) => {
 		if (!groups.length) return resolve();
 		// Fetch licenses and classes
@@ -56,7 +56,7 @@ demolish.groups = (groups = [], licenses = [], classes = []) => {
 		// Delete the classes associated with the group
 		if (classes.length) {
 			try {
-				await demolish.classes(classes, licenses);
+				await demolish.classes(classes, groups, licenses, process);
 			} catch (error) {
 				return reject(error);
 			}
@@ -64,7 +64,7 @@ demolish.groups = (groups = [], licenses = [], classes = []) => {
 		// Delete the licenses associated with the group
 		if (licenses.length) {
 			try {
-				await demolish.licenses(licenses);
+				await demolish.licenses(licenses, groups, classes, process);
 			} catch (error) {
 				return reject(error);
 			}
@@ -80,17 +80,92 @@ demolish.groups = (groups = [], licenses = [], classes = []) => {
 	});
 };
 
-demolish.classes = (classes = [], licenses = []) => {
+demolish.classes = (classes = [], groups = [], licenses = [], process = "delete class") => {
 	return new Promise(async (resolve, reject) => {
 		if (!classes.length) return resolve();
+		if (process === "delete class") {
+			// Fetch groups
+			// TODO: Validated inputted groups. Does it match with the groups' licenses?
+			if (!groups.length) {
+				let groupIds = [];
+				for (let h = 0; h < classes.length; h++) {
+					groupIds.push(classes[h].group);
+				}
+				try {
+					groups = await Group.find({ _id: groupIds });
+				} catch (error) {
+					return reject({ status: "error", content: error });
+				}
+			}
+			// Fetch licenses
+			// TODO: Validated inputted licenses. Does it match with the classes' licenses?
+			if (!licenses.length) {
+				let licenseIds = [];
+				for (let i = 0; i < classes.length; i++) {
+					licenseIds = licenseIds.concat(classes[i].licenses.active);
+					licenseIds = licenseIds.concat(classes[i].licenses.requested);
+					licenseIds = licenseIds.concat(classes[i].licenses.invited);
+				}
+				if (licenseIds.length) {
+					try {
+						licenses = await License.find({ _id: licenseIds });
+					} catch (error) {
+						return reject({ status: "error", content: error });
+					}
+				}
+			}
+			let promises = [];
+			// Remove the linkage between groups and classes
+			for (let j = 0; j < classes.length; j++) {
+				const instance = classes[j];
+				// Find the group of interest
+				let group = groups.find((group) => group._id.toString() === instance._id.toString());
+				// Remove the class ID from the group's classes array
+				group.classes = group.classes.filter((classId) => {
+					return classId.toString() !== instance._id.toString();
+				});
+				// Save changes
+				group.date.modified = new Date().toString();
+				promises.push(group.save());
+			}
+			// Remove the linkage between licenses and classes
+			if (licenses) {
+				// For each license of each class, remove the class ID from its classes array
+				for (let k = 0; k < classes.length; k++) {
+					const instance = classes[k];
+					// Retrieve all the licenses associated with the class
+					let instanceLicenses = licenses.filter((license) => {
+						return license.classes.some((classId) => classId.toString() === instance._id.toString());
+					});
+					// For each license, remove the class ID
+					for (let l = 0; l < instanceLicenses.length; l++) {
+						let license = instanceLicenses[l];
+						// Remove the class ID from its classes array
+						license.classes = license.classes.filter((classId) => {
+							return classId.toString() !== instance._id.toString();
+						});
+						// Save changes
+						license.date.modified = new Date().toString();
+						promises.push(license.save());
+					}
+				}
+			}
+		}
+		// Delete the class
+		try {
+			await Class.deleteMany({ _id: classes.map((element) => element._id) });
+		} catch (error) {
+			return reject({ status: "error", content: error });
+		}
 		// Success handler
 		return resolve();
 	});
 };
 
-demolish.licenses = (licenses = [], option) => {
+demolish.licenses = (licenses = [], groups = [], classes = [], process = "delete license") => {
 	return new Promise(async (resolve, reject) => {
 		if (!licenses.length) return resolve();
+
 		// Success handler
 		return resolve();
 	});
